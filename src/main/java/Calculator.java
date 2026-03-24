ac8db72aa360a1cf905fbc78be29691864da2994
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-03-24 14:47:50 +0100

    petites modifs sur la class Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,21 +1,17 @@
 public class Calculator {
 
 public int addNumber(int x ,int y){
-   int sum =  x + y;
-   return sum;
+     return x + y;
 }
 
 public int subtraction(int x ,int y){
-    int sum = x - y;
-    return sum;
+       return x - y;
 }
 public int multiplication(int x, int y){
-    int sum = x * y;
-    return sum;
+      return x * y;
 }
 public int division(int x, int y){
-    int sum = x / y;
-    return sum;
+    return  x / y;
 }
 }
 
